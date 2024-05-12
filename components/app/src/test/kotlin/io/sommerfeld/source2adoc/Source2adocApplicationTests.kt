fd26d0788b2ee201047086265f380fb6523377d6
sebastian <sebastian@sommerfeld.io>
2024-05-12 20:50:29 +0000

    kotlin-ready dev-comtainer

---
+++ components/app/src/test/kotlin/io/sommerfeld/source2adoc/Source2adocApplicationTests.kt
@@ -0,0 +1,13 @@
+package io.sommerfeld.source2adoc
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class Source2adocApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
