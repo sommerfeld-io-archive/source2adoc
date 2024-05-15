8d3a14b417aa54ae6ca63c6d59bbeba3153e8ed1
sebastian <sebastian@sommerfeld.io>
2024-05-15 20:50:49 +0000

    introduce new unit test case

---
+++ components/app/internal/helpers_test.go
@@ -0,0 +1,19 @@
+package internal
+
+import (
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestCurrentWorkingDir(t *testing.T) {
+	expected, err := os.Getwd()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	dir := CurrentWorkingDir()
+
+	assert.Equal(t, expected, dir, "Should return the correct current working directory")
+}
