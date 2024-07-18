85bb39dfba0f475d9608885bcc7b53a77c88adc3
sebastian <sebastian@sommerfeld.io>
2024-07-18 15:15:10 +0000

    chore: setup basic kotlin project structure

---
+++ components/app/src/main/kotlin/io/sommerfeld/source2adoc/Source2adocApplication.kt
@@ -0,0 +1,11 @@
+package io.sommerfeld.source2adoc
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Source2adocApplication
+
+fun main(args: Array<String>) {
+	runApplication<Source2adocApplication>(*args)
+}
