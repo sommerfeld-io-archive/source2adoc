3b8266714b66dcb8e32a029ece59bf06b4be0c0e
sebastian <sebastian@sommerfeld.io>
2024-07-18 21:10:39 +0000

    refactor: minor update

--- components/app/src/main/kotlin/io/sommerfeld/source2adoc/shell/Calculator.kt
+++ components/app/src/main/kotlin/io/sommerfeld/source2adoc/shell/Calculator.kt
@@ -2,17 +2,16 @@ package io.sommerfeld.source2adoc.shell
 
 import org.springframework.shell.standard.ShellComponent
 import org.springframework.shell.standard.ShellMethod
-import org.springframework.shell.standard.ShellOption
 
 @ShellComponent
-class Calculator {
+public class Calculator {
 
-    @ShellMethod("Add two integers together.")
+    @ShellMethod("Add two integers together")
     fun plus(a: Int, b: Int): Int {
         return a + b
     }
 
-    @ShellMethod("Subtract one integer from another.")
+    @ShellMethod("Subtract one integer from another")
     fun minus(a: Int, b: Int): Int {
         return a - b
     }
