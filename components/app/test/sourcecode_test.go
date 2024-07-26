c29ae330e47a14a3024816bf7dc353844651539e
sebastian <sebastian@sommerfeld.io>
2024-07-26 09:36:49 +0000

    ci: lint go code pre commit and in pipeline

--- components/app/test/sourcecode_test.go
+++ components/app/test/sourcecode_test.go
@@ -1,16 +1,1 @@
 package test
-
-// // See https://github.com/kcmvp/archunit
-
-// import (
-// 	"testing"
-
-// 	"github.com/stretchr/testify/assert"
-// )
-
-// func TestAllPackages(t *testing.T) {
-// 	assert := assert.New(t)
-
-// 	err := AllPackages().NameShouldBeSameAsFolder()
-// 	assert.NotNil(err)
-// }
