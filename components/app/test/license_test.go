ca582f51a5940103a11dcfc51011f1c392af6019
sebastian <sebastian@sommerfeld.io>
2024-08-19 06:37:06 +0000

    refactor: remove obsolete file

--- components/app/test/license_test.go
+++
@@ -1,7 +0,0 @@
-package test
-
-// Test the licenses of all Go modules and dependencies in the project.
-// Make sure the licenses are compatible with the project's MIT license.
-
-// TODO: check here as well? by running go-licenses from a test. Or is
-// TODO: that too much? Maybe just run it in CI.
