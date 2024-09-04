39b872221b2eca11acb136b18f943fda2ddabb06
sebastian <sebastian@sommerfeld.io>
2024-09-04 13:05:27 +0000

    refactor: remove obsolete tool installations

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -6,15 +6,3 @@
 
 echo "[INFO] Initialize pre-commit"
 pre-commit install
-
-echo "[INFO] Install gocyclo"
-readonly components=(
-    /workspaces/source2adoc/components/app
-    /workspaces/source2adoc/components/test-acceptance
-)
-for dir in "${components[@]}"; do
-    (
-        cd "$dir" || exit
-        go install github.com/fzipp/gocyclo/cmd/gocyclo@latest
-    )
-done
